f5491ea74137a7cb3f51f9dab272efe1eb755c65
Srinu Jella <sjella@codeaurora.org>
2020-04-08 11:22:08 +0530

    rolex: Add support of non offload by default
    - Based on Andriod version pick the right non offload
      xml file as it is shared project.
    
    CRs-Fixed: 2469019
    Change-Id: I37e3ea8eef4a93201cdc6b199a5c69547d13f243
    
    Signed-off-by: Thagoo <lohitgowda56@gmail.com>

--- device.mk
+++ device.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    $(DEVICE_PATH)/configs/bluetooth_qti_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
